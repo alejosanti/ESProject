99fd2452ad675cbea6d2644f1165247d81650f99
alejosanti <50599731+alejosanti@users.noreply.github.com>
2022-04-22 21:45:09 -0300

    Update otaesp.ino

--- Arduino_code/otaesp/otaesp.ino
+++ Arduino_code/otaesp/otaesp.ino
@@ -7,7 +7,7 @@
 /*
  * usuario y contrasenia par ala conexión al ESP en modo AP
  */
-const char* ssid = "ESP32_AP ";
+const char* ssid = "ESP32_AP";
 const char* password = "123456789";
 const char* version = "v3.0.0";
 bool updateIsOk = true;
